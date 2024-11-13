18caba33cc380cee4f2cbde1fb578b39655f998d
t1mmuz <1423766572@qq.com>
2024-11-13 18:38:53 +0800

    lab3 finish

--- lab/lab2/sponge/libsponge/network_interface.cc
+++ lab/lab2/sponge/libsponge/network_interface.cc
@@ -35,7 +35,7 @@ void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Addres
 
     auto it = _arp_table.find(next_hop_ip);
     if (it != _arp_table.end()) {
-        // ARP 表命中，则直接发送 IP 数据报
+        // hit
         _send(it->second.eth_addr, EthernetHeader::TYPE_IPv4, dgram.serialize());
     } else {
         // ARP 表不命中，且最近没有对该 IP 发送过 ARP 查询数据报，则发送查询报文
@@ -49,10 +49,9 @@ void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Addres
 
             _send(ETHERNET_BROADCAST, EthernetHeader::TYPE_ARP, arp_msg.serialize());
 
-            // 加入等待回复 ARP 报文的记录表中
             _waiting_arp_response_ip_addr[next_hop_ip] = ARP_RESPONSE_TTL_MS;
         }
-        // 加入 IP 报文等待发送列表
+        
         _waiting_internet_datagrams[next_hop_ip].emplace_back(next_hop, dgram);
     }
 
@@ -60,25 +59,25 @@ void NetworkInterface::send_datagram(const InternetDatagram &dgram, const Addres
 
 //! \param[in] frame the incoming Ethernet frame
 optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &frame) {
-    // 如果不是广播帧或者 MAC 地址不是本机，则直接过滤
+    // filter
     if (frame.header().dst != ETHERNET_BROADCAST && frame.header().dst != _ethernet_address) {
         return nullopt;
     }
-    // 如果是 IPv4 数据报，则把解析后把数据返回
+    // ipv4
     if (frame.header().type == EthernetHeader::TYPE_IPv4) {
         InternetDatagram ret;
-        // 解析成功则返回
+        // success
         if (ret.parse(frame.payload()) == ParseResult::NoError) return ret;
         return nullopt;
     }
-    // 如果是 ARP 数据报，则尝试从中学习到 IP-MAC 映射关系（我们可以同时从 ARP 请求和响应包中获取到新的 ARP 表项）
+    // ip-mac
     if (frame.header().type == EthernetHeader::TYPE_ARP) {
         ARPMessage arp_msg;
         if (arp_msg.parse(frame.payload()) != ParseResult::NoError) return nullopt;
 
         const uint32_t my_ip = _ip_address.ipv4_numeric();
         const uint32_t src_ip = arp_msg.sender_ip_address;
-        // 如果是发给本机的 ARP 请求
+        
         if (arp_msg.opcode == ARPMessage::OPCODE_REQUEST && arp_msg.target_ip_address == my_ip) {
             ARPMessage arp_reply;
 
@@ -90,14 +89,12 @@ optional<InternetDatagram> NetworkInterface::recv_frame(const EthernetFrame &fra
 
             _send(arp_msg.sender_ethernet_address, EthernetHeader::TYPE_ARP, arp_reply.serialize());
         }
-        // 从 ARP 报文中学习新的 ARP 表项（即使不是发给我的也可以学，比如广播但目标 IP 不是本机）
         _arp_table[src_ip] = {arp_msg.sender_ethernet_address,  ARP_ENTRY_TTL_MS};
         
-        // 如果该 IP 地址有等待发送的数据报，则全部发送出去
         auto it = _waiting_internet_datagrams.find(src_ip);
+
         if (it != _waiting_internet_datagrams.end()) {
             for (const auto &[next_hop, dgram] : it->second) {
-                // send_datagram(dgram, next_hop);
                 _send(arp_msg.sender_ethernet_address, EthernetHeader::TYPE_IPv4, dgram.serialize());
             }
             _waiting_internet_datagrams.erase(it);
@@ -121,8 +118,8 @@ void NetworkInterface::tick(const size_t ms_since_last_tick) {
     // 删除等待 ARP 报文回复超时的记录，不作重发处理
     for (auto it = _waiting_arp_response_ip_addr.begin(); it != _waiting_arp_response_ip_addr.end(); ) {
         if (it->second <= ms_since_last_tick) {
-            // 如果还有等待发往该未回复的 IP 地址的数据报，则直接丢弃
             auto it2 = _waiting_internet_datagrams.find(it->first);
+
             if (it2 != _waiting_internet_datagrams.end())
                 _waiting_internet_datagrams.erase(it2);
 
